b214a99f05c3e36de557f40addc972b319a6778a
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-12-21 19:50:58 -0300

    create Calculator const file & basic math operations

---
+++ Calculator.js
@@ -0,0 +1,14 @@
+class Calculator {
+  add(n1, n2) {
+    return n1 + n2;
+  };
+  substract(n1, n2) {
+    return n1 - n2;
+  };
+  multiply(n1, n2) {
+    return n1 * n2;
+  };
+  divide(n1, n2) {
+    return n1 / n2;
+  };
+}
