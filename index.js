a7a21eb20e95270a9d641838622c5b4346647a87
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-12-21 19:35:35 -0300

    create vars

---
+++ index.js
@@ -0,0 +1,6 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const btnNumber = document.querySelectorAll('.numero');
+const btnOperators = document.querySelectorAll('.operador');
+
+
