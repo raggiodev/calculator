a743736a7c6965de2a11583e63e931e69c595e9a
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-12-22 18:09:42 -0300

    finish calculator

--- Display.js
+++ Display.js
@@ -5,19 +5,13 @@ class Display {
     this.calculator = new Calculator();
     this.OperatorType = undefined;
     this.valorActual = '';
-    this.calorAnterior = '';
-  }
-
-  addNumber(n) {
-    if(n === '.' && this.valorActual.includes('.')) return
-
-    this.valorActual = this.valorActual.toString() + n.toString();
-    this.printValue();
-  }
-
-  printValue() {
-    this.displayValorActual.textContent = this.valorActual;
-    this.displayValorAnterior.textContent = this.valorAnterior;
+    this.valorAnterior = '';
+    this.symbols = {
+      add: '+',
+      substract: '-', 
+      multiply: 'x',
+      divide: '%',
+    }
   }
 
   delete() {
@@ -31,6 +25,27 @@ class Display {
     this.OperatorType = undefined;
     this.printValue();
   }
+
+  compute(type) {
+    this.OperatorType !== 'equal' && this.calc();
+    this.OperatorType = type;
+    this.valorAnterior = this.valorActual || this.valorAnterior;
+    this.valorActual = '';
+    this.printValue();
+  }
+
+  addNumber(n) {
+    if(n === '.' && this.valorActual.includes('.')) return
+
+    this.valorActual = this.valorActual.toString() + n.toString();
+    this.printValue();
+  }
+
+  printValue() {
+    this.displayValorActual.textContent = this.valorActual;
+    this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.symbols[this.OperatorType] || ''}`;
+  }
+
 
   calc() {
     const valorAnterior = parseFloat(this.valorAnterior);
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const displayValorAnterior = document.getElementById('valor-anterior');
 const displayValorActual = document.getElementById('valor-actual');
 const btnNumber = document.querySelectorAll('.numero');
-const btnOperators = document.querySelectorAll('.operador');
+const btnOperator = document.querySelectorAll('.operador');
 
 const display = new Display(displayValorAnterior, displayValorActual); 
 
 btnNumber.forEach(btn => {
   btn.addEventListener('click', () => display.addNumber(btn.innerHTML));
+})
+
+btnOperator.forEach(btn => {
+  btn.addEventListener('click', () => display.compute(btn.value));
 })
